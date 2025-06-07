3e720b81ed87ba21e76a83d17ec557ba900bd344
mbongoelvis <elvis.moribindaka@digisolgroup.net>
2025-06-07 11:15:01 +0100

    change the varables for the backend

--- db_connect.js
+++ db_connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_CONNECT);
+    await mongoose.connect(process.env.ONLINE_DB);
     console.log("Database connection established");
   } catch (err) {
     console.error("Database connection error:", err.message);
